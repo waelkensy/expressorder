295c4a80665604325ff17924a3fdb85dd66e698b
Yannick Waelkens <yannick.waelkens@cgi.com>
2020-08-14 18:49:56 +0200

    add express order on cart page

---
+++ Block/Cart/ExpressOrder.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Wlks\ExpressOrder\Block\Cart;
+
+
+use Magento\Checkout\Block\Cart\AbstractCart;
+
+class ExpressOrder extends AbstractCart
+{
+
+}
---
+++ Controller/Cart/Expressorder.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Wlks\ExpressOrder\Controller\Cart;
+
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Checkout\Model\Cart as CustomerCart;
+use Magento\Framework\App\Action\HttpPostActionInterface as HttpPostActionInterface;
+
+class Expressorder extends \Magento\Checkout\Controller\Cart\Add implements HttpPostActionInterface
+{
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Framework\Data\Form\FormKey\Validator $formKeyValidator,
+        CustomerCart $cart,
+        ProductRepositoryInterface $productRepository
+    ) {
+        parent::__construct(
+            $context,
+            $scopeConfig,
+            $checkoutSession,
+            $storeManager,
+            $formKeyValidator,
+            $cart,
+            $productRepository
+        );
+    }
+
+    public function execute()
+    {
+        /* @TODO ADD FORMKEY VALIDATOR */
+
+        $params = $this->getRequest()->getParams();
+        if (isset($params)) {
+            try {
+                $product = $this->_initProduct();
+                if (!$product) {
+                    return $this->goBack();
+                }
+
+                $this->cart->addProduct($product, ['qty' => '1']);
+                $this->cart->save();
+
+                $this->_eventManager->dispatch(
+                    'checkout_cart_add_product_complete',
+                    ['product' => $product, 'request' => $this->getRequest(), 'response' => $this->getResponse()]
+                );
+
+                if (!$this->_checkoutSession->getNoCartRedirect(true)) {
+                    if (!$this->cart->getQuote()->getHasError()) {
+                        $message = __(
+                            'You added %1 to your shopping cart.',
+                            $product->getName()
+                        );
+                        $this->messageManager->addSuccessMessage($message);
+                    }
+                }
+
+                return $this->goBack(null, $product);
+            } catch (\Exception $exception) {
+                var_dump($exception->getMessage()); die;
+                $this->messageManager->addExceptionMessage(
+                    $exception,
+                    __('We can\'t add this item to your shopping cart right now.')
+                );
+                $this->_objectManager->get(\Psr\Log\LoggerInterface::class)->critical($exception);
+                return $this->goBack();
+            }
+        }
+    }
+
+}
